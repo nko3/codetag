fbe13a3f929c6310c96a905d168bd2735409862f
Agares <ekeisco@gmail.com>
2012-11-09 18:39:16 -0500

    first commit, base tcp connection for the chat

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
+var net = require('net');
+var server = net.createServer(function (socket) {
+  socket.on("connect", function(){
+    socket.write("conectado al chat");
+  });
+  socket.on("data",function(data){
+    socket.write("ud envio: "+data);
+    if(data.toString().trim()=="salir"){
+        socket.end("chaolin");
+    }
+  });
+  socket.on("end",function(){
+  });
+  socket.on("close",function(){
+  });
+  socket.on("timeout",function(){
+  });
+})
+server.listen(5555);
 
-console.log('Server running at http://0.0.0.0:8000/');
+console.log('Server running at telnet localhost 5555');
