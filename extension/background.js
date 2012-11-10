65644353072cd37a8a04d1f825bc43c97fe260a4
kuryaki <david.roncancio@gmail.com>
2012-11-10 06:45:51 -0500

    No more popups it works from a background javascript

--- extension/background.js
+++ extension/background.js
@@ -1,1 +1,10 @@
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.getSelected(undefined, function(tab){ 
+    chrome.tabs.executeScript(null,{file:"socket.io.js"}); //cargo los js en el ambito de la pagina
+    chrome.tabs.executeScript(null,{file:"chatroom.js"});
+  });
+});
+var port = chrome.extension.connect();
+
 console.log("this is running in background");
--- extension/chat.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  chrome.tabs.getSelected(undefined, function(tab){
-  	chrome.tabs.executeScript(null,{file:"socket.io.js"}); //cargo los js en el ambito de la pagina
-    chrome.tabs.executeScript(null,{file:"chatroom.js"});
-  });
-});
-
-var port = chrome.extension.connect();
-
