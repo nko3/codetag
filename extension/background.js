1c908f669b940880afb52c044a2b118ed4ac1c1a
Giovanny <ekeisco@gmail.com>
2012-11-11 11:25:27 -0500

    first version of the badge

--- extension/background.js
+++ extension/background.js
@@ -4,6 +4,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null,{file:"chatroom.js"});
   });
 });
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	var x = parseInt(Math.random(100)*100);
+	x = ""+x;
+	chrome.browserAction.setBadgeText({text:x,tabId:activeInfo.tabId});
+});
+
 var port = chrome.extension.connect();
 
 console.log("this is running in background");
