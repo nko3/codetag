96e8e0e5ba04983a6fac7da4efde94af7eed0204
Giovanny <ekeisco@gmail.com>
2012-11-10 22:22:13 -0500

    close the chat issue

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,5 @@
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.getSelected(undefined, function(tab){ 
+  chrome.tabs.getSelected(undefined, function(tab){
     chrome.tabs.executeScript(null,{file:"socket.io.js"}); //cargo los js en el ambito de la pagina
     chrome.tabs.executeScript(null,{file:"chatroom.js"});
   });
