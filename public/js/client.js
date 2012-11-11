1f960d5d2b0466987f93fe5cd4b5d818dd88afb2
Giovanny <ekeisco@gmail.com>
2012-11-11 02:38:56 -0500

    chat icon

--- public/js/client.js
+++ public/js/client.js
@@ -28,8 +28,12 @@ $('#minimize').click(function(){
     console.log(chatSite.height());
     if(chatSite.height()!=20){
         chatSite.height("20px");
+        $('#minimize').attr('title', 'show');
+        $('#minimize').html('+');
     }else{
         chatSite.height("320px");
+        $('#minimize').attr('title', 'minimize');
+        $('#minimize').html('_');
     }
     return false;
 });
